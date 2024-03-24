f1777449e01f4faa657826cb207bbe07b283201f
Hieu Pham <hieuminhphamcse21@gmail.com>
2024-03-24 10:45:55 -0400

    initialize project

---
+++ src/main/java/com/hpham/springmultimoduledemo/SpringMultimoduleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hpham.springmultimoduledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultimoduleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultimoduleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hpham/springmultimoduledemo/SpringMultimoduleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpham.springmultimoduledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultimoduleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
